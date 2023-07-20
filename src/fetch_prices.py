204d95eef26135b183fa740b888a0ba4ac352385
Ubuntu <ubuntu@ip-172-31-17-70.eu-west-3.compute.internal>
2023-07-20 01:45:56 +0000

    Ajout d'un bot v2 en Orienté Objet

--- src/fetch_prices.py
+++ src/fetch_prices.py
@@ -1,107 +1,131 @@
+# Fetch coin data
 import requests
 import json
-import pprint
+from datetime import datetime
 
+class CoinDataFetcher :
 
-url = "https://api.coingecko.com/api/v3/coins/bitcoin"
-params = {
-        "localization":"fr",
-        "tickers": "true",
-        "market_data":"true",
-        "community_data":"true"
-        } 
+  def __init__(self, coin_id):
+    self.coin_id = coin_id
 
-response = requests.get(url,params)
+  # Fetch coin data
+  def get_coin_data(self):
+    url = f"https://api.coingecko.com/api/v3/coins/{self.coin_id}"
+    params = {
+      "localization":"fr",  
+      "tickers":"true",
+      "market_data":"true",
+      "community_data":"true"
+    }
 
-data = json.loads(response.text)
+    response = requests.get(url, params=params)
+    return json.loads(response.text)
 
-description = data["description"]["sl"]
-genesis = data["genesis_date"]
+  # Get description
+  def get_description(self, data):
+    return data["description"]["en"]
 
+  # Get genesis date
+  def get_genesis_date(self, data):
+    return data["genesis_date"]
+
+  # Get market data
+  def get_market_data(self, data):
+    return data["market_data"]
+
+  # Get current price
+  def get_current_price(self, market_data):
+    return market_data["current_price"]["eur"]
+  
+# Get current timestamp
+  def get_timestamp(self, market_data):
+      return market_data["last_updated"]
+
+  # Get ATH details
+  def get_ath_data(self, market_data):
+    return {
+      "ath": market_data["ath"]["eur"],
+      "ath_change": market_data["ath_change_percentage"]["eur"],
+      "ath_date": market_data["ath_date"]["eur"]
+    }
+
+  # Get fully diluted valuation
+  def get_fdv(self, market_data):
+    return market_data["fully_diluted_valuation"]["eur"]  
+
+  # Get percentage changes
+  def get_percentage_changes(self, market_data):
+    return {
+      "1h": market_data["price_change_percentage_1h_in_currency"]["eur"],
+      "24h": market_data["price_change_percentage_24h_in_currency"]["eur"],
+      "7d": market_data["price_change_percentage_7d_in_currency"]["eur"],
+      "14d":market_data["price_change_percentage_14d_in_currency"]["eur"],
+      "30d":market_data["price_change_percentage_30d_in_currency"]["eur"],
+      "200d":market_data["price_change_percentage_200d_in_currency"]["eur"],
+      "1y":market_data["price_change_percentage_1y_in_currency"]["eur"],
+    }
+
+  # Get circulating supply
+  def get_circulating_supply(self, market_data):
+    return market_data["circulating_supply"]
+
+  # Get last updated
+  def get_last_updated(self, market_data):
+    return market_data["last_updated"]
+
+  # Get developer data
+  def get_dev_data(self, data):
+    return data["developer_data"]
+  
+  def get_public(self, data):
+      return data["public_interest_stats"]
+
+  def get_tickers(self, data):
+    return data['tickers']
+
+  # Get exchange volumes
+#  def get_exchange_volumes(self, tickers):
+#    volumes = []
+#    for t in tickers:
+#      pair = f"{t['base']}/{t['target']}"
+#      volume = {
+#        "exchange": t["market"]["name"], 
+#        "volume": t["volume"],
+#        "pair": pair
+#      }
+#      volumes.append(volume)
+#    return volumes
+
+  def get_all_volumes(self, tickers):
+    volumes = []
+    for t in tickers:
+        pair = f"{t['base']}/{t['target']}"
+        volume = {
+                "exchange": t["market"]["name"],
+                "volume": t["volume"], 
+                "trust_score": t["trust_score"],
+                "last_trade": t["last_traded_at"],
+                "url": t["trade_url"],
+                "pair": pair
+                }
+        volumes.append(volume)
+    return volumes
+  
+  def get_major_volumes(self, all_volumes):
+    sorted_volumes = sorted(all_volumes, key=lambda x: x["volume"], reverse=True)
+    return sorted_volumes[:5]
+
+  def get_kraken_volumes(self, all_volumes):
+    return [v for v in all_volumes if v["exchange"] == "Kraken"]
+  
+  def get_binance_volumes(self, all_volumes):
+    return [v for v in all_volumes if v["exchange"] == "Binance"]
+
+# Usage
+coin_id = "bitcoin"
+fetcher = CoinDataFetcher(coin_id)
+data = fetcher.get_coin_data()
+description = fetcher.get_description(data)
 market_data = data["market_data"]
-
-price = data["market_data"]["current_price"]["eur"] 
-
-ath = market_data["ath"]["eur"]
-
-change_from_ath = market_data["ath_change_percentage"]["eur"]
-
-ath_date = market_data["ath_date"]["eur"]
-
-fully_diluted_valuation = market_data["fully_diluted_valuation"]["eur"]
-
-fully_diluted_valuation = "{:=,}".format(fully_diluted_valuation)
-
-
-percent_change_1h = market_data["price_change_percentage_1h_in_currency"]["eur"]
-percent_change_24h = market_data["price_change_percentage_24h_in_currency"]["eur"]
-percent_change_7d = market_data["price_change_percentage_7d_in_currency"]["eur"]
-percent_change_14d = market_data["price_change_percentage_14d_in_currency"]["eur"]
-percent_change_30d = market_data["price_change_percentage_30d_in_currency"]["eur"]
-percent_change_200d = market_data["price_change_percentage_200d_in_currency"]["eur"]
-percent_change_1y = market_data["price_change_percentage_1y_in_currency"]["eur"]
-
-actual_supply = market_data["circulating_supply"]
-
-timestamp = market_data["last_updated"]
-
-dev_data = data["developer_data"]
-
-
-forks = dev_data["forks"]
-stars = dev_data["stars"]
-subs = dev_data["subscribers"]
-total_issues = dev_data["total_issues"]
-pull_requests = dev_data["pull_requests_merged"]
-contributors = dev_data["pull_request_contributors"]
-additions_deletions = dev_data["code_additions_deletions_4_weeks"] #{ "additions": 4932,"deletions": -1770}
-commit_4w = dev_data["commit_count_4_weeks"]
-
-public = data["public_interest_stats"]
-
-
-# Compute 5 major exchange in volume
-
-tickers = data["tickers"]
-
-number_exchanges = len(tickers)
-all_volumes_dict = []
-
-for exchange in range(number_exchanges):
-    pair = f"{tickers[exchange]['base']}/{ tickers[exchange]['target']}"
-    infos = {"exchange": tickers[exchange]["market"]["name"], "volume" : tickers[exchange]["volume"], "coin gecko score" : tickers[exchange]["trust_score"], "last trade": tickers[exchange]["last_traded_at"], "url" : tickers[exchange]["trade_url"], "pair" : pair}
-    
-    all_volumes_dict.append(infos)
-
-
-all_volumes_sorted = sorted(all_volumes_dict, key=lambda x:x["volume"], reverse=True)
-
-
-#print("\n ------------------- FIVE MAJORS VOLUME ---------------- \n ")
-
-major_volumes = [all_volumes_sorted[index] for index in range(5)]
-
-
-#major_exchanges = [major_volumes[index][index] for index in range(5)]
-
-#pprint.pprint(major_volumes)
-
-#print("Five major exchange : ", major_exchanges)
-
-#print("\n -------------------- KRAKEN ------------------------ \n ")
-
-indices_kraken = [i for i, item in enumerate(all_volumes_dict) if item["exchange"] == "Kraken"]
-kraken = [all_volumes_dict[index] for index in indices_kraken]
-
-#pprint.pprint(kraken)
-
-#print("\n ----------------------------- BINANCE -------------------- \n")
-
-indices_binance = [i for i, item in enumerate(all_volumes_dict) if item["exchange"] == "Binance"]
-binance = [all_volumes_dict[index] for index in indices_binance]
-
-#pprint.pprint(binance)
-
-
-
-
+timestamp = fetcher.get_timestamp(market_data)
+print("At ", timestamp, "    ",description)
