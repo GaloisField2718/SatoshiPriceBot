7061b2d786993dcc7a19883ae7384311b264edf8
GaloisField2718 <galoisfield2718@gmail.com>
2023-11-02 21:03:36 +0000

    Possible usage of float sats/token values. Update for /getlastprice

--- src/oshi_plus.py
+++ src/oshi_plus.py
@@ -11,17 +11,25 @@ def fetch(token):
     driver = webdriver.Chrome(options=options)
     driver.get(f'https://unisat.io/market/brc20?tick={token}')
     wait = WebDriverWait(driver, 10)
-    price_sats = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "price")))
+    wait.until(EC.presence_of_element_located((By.CLASS_NAME, "price")))
     price_sats = driver.find_element(By.XPATH, '//div[contains(@class, "price-line")]/span[contains(@class, "price")]')
-    amount_oshi = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "inscription-name  ")))
-    amount_oshi = driver.find_element(By.XPATH, '//div[contains(@class, "content display-domain white")]/div[contains(@class, "inscription-name  ")]')
-    price_usd = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "usd")))
+    try :
+        wait.until(EC.presence_of_element_located((By.CLASS_NAME, "inscription-name  ")))
+        amount_oshi = driver.find_element(By.XPATH, '//div[contains(@class, "content display-domain white")]/div[contains(@class, "inscription-name")]')
+    except ERROR:
+        wait.unit(EC.presence_of_element_located((By.CLASS_NAME, "inscription-name too-long")))
+        amount_oshi = driver.find_element(By.XPATH,'//div[contains(@class, "content display-domain white")]/div[contains(@class, "inscription-name too-long")]')
+ 
+    wait.until(EC.presence_of_element_located((By.CLASS_NAME, "usd")))
     price_usd = driver.find_element(By.XPATH, '//div[contains(@class, "price-line")]/span[contains(@class, "usd")]')
     xpath = "//*[@id='rc-tabs-0-panel-1']/div/div/div[1]/div[2]/div[3]/span[1]"
     price_btc = wait.until(EC.presence_of_element_located((By.XPATH, xpath)))
     price_btc = driver.find_element(By.XPATH, xpath)
-    
-    price_sats = int(price_sats.text)
+    try :
+        price_sats = int(price_sats.text)
+    except ValueError:
+        price_sats = float(price_sats.text)
+
     amount_oshi = (amount_oshi.text).replace(',','')
     amount_oshi = int(amount_oshi)
     price_usd = price_usd.text
--- src/qr_generator/qr.py
+++ src/qr_generator/qr.py
@@ -1,7 +1,11 @@
 import qrcode
+import sys
 
+data = sys.argv[1]
 qr = qrcode.QRCode(border=4, box_size=10)
-qr.add_data('bc1qxxuuxmlp3wxuyn6uuqw448nzaafuqdxc076m9k')
+qr.add_data(data)
+
 qr.make(fit=True)
 img = qr.make_image(fill_color="white", back_color="black")
-img.save('qr_myaddress.png')
+img.save(f'data.png')
+
